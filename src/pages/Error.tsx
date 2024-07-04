9d7725e37fca842ba33f246f145967214c862694
jpatrickldg <jpatrickldg@gmail.com>
2024-07-04 17:32:45 +0800

    add login and custom error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,4 +1,26 @@
+import {
+  Link,
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError,
+} from "react-router-dom";
+
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+  const navigate = useNavigate();
+  return (
+    <div>
+      <h1>Uh oh! We've got a problem.</h1>
+      <p>
+        {isRouteErrorResponse(error)
+          ? error.data.message || error.statusText
+          : "Unknown error message."}
+      </p>
+      <div>
+        <button onClick={() => navigate(-1)}>Go Back</button>
+        <Link to="/">Go Home</Link>
+      </div>
+    </div>
+  );
 };
 export default Error;
